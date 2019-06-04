2df637e1c0b9e741449bffa16537cd9e2e1e142c
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-06-04 18:41:31 -0500

    done with exercises

---
+++ script.js
@@ -0,0 +1,44 @@
+const volunteerData = [
+  {
+    congressionalDistict: 4
+  },
+  {
+    platformStatements: {
+      taxes: "statement",
+      jobs: "statement",
+      infrastructure: "statement",
+      healthCare: "statement",
+      crimeAndEnforcement: "statement"
+    }
+  },
+  {
+    donationURL: "givememoney.com"
+  },
+  {
+    volunteerInfo: {
+      name: "name",
+      address: "address",
+      email: "email",
+      phoneNumber: "phone number",
+      availability: "none",
+      activities: ["answering phone calls", "taking polls"]
+    }
+  },
+  {
+    biography: "info"
+  },
+  {
+    imageGallery: {
+      headShot: "img",
+      pictureOfFam: "img",
+      pictureOfVoters: "img"
+    }
+  },
+  {
+    missionStatement: "statement"
+  },
+  {
+    URLForVoters: "URL"
+  }
+];
+console.log(volunteerData);
